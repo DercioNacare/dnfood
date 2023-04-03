b4a8fba3a2dc1aa17cdd513072f6f317d874d4c6
Dercio Nacare <dpvnacare@gmail.com>
2023-04-03 12:52:14 +0200

    Adicionando JPA e configurando o Data Source

---
+++ src/main/java/com/dncode/dnfood/DnfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.dncode.dnfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dncode/dnfood/DnfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dncode.dnfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
